16e7c8e5560a066693f53e38ec7480ec06ae285b
WarBuggy <hovanbuu@gmail.com>
2024-05-24 18:08:58 -0700

    * Basic backend files

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const app = express();
+const portNumber = 8566;
+
+start();
+
+async function start() {
+    app.use(express.json());
+    app.use(express.urlencoded({ limit: '10mb', extended: false, }));
+    app.use(cors());
+    http.createServer(app).listen(portNumber, function () {
+        console.log(`HTTP Server started on port ${portNumber}.`);
+        require('./api.js')(app);
+    });
+};
